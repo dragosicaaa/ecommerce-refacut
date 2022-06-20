1bfb26c0f444dc218e21eea8fe7a55f524a758b5
Your Name <you@example.com>
2022-06-20 10:46:01 +0300

    refacut

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">My website</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
